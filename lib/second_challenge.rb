3668db729cd2c2ef98d3ae7977c9bf50b74637e9
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-07 17:48:45 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -6,7 +6,7 @@ def second_challenge
       hero: {},
       hero_friends: []
       #your key/value pairs here!
-      }, 
+      },
     :capulet => {
       patriarch: {},
       matriarch: {},
@@ -14,5 +14,5 @@ def second_challenge
       heroine_friends: []
       }}
 
-  
+
 end
