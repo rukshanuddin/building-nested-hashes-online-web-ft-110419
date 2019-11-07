4924d21a011ed07d0aeb69bf0f85210aa5d03ce7
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-07 17:54:17 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,35 +1,15 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
-        name: "",
-        age: "",
-        },
-      :matriarch => {
-        name: "",
-        age: "",
-        },
-      :hero => {
-        name: "",
-        age: "",
-        status: "",
-        },
+     :patriarch => {name: "Lord Montague", age: "53"},
+     :matriarch => {name: "Lady Montague", age: "54"},
+     :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => []
    },
    :capulet => {
-      :patriarch => {
-        name: "",
-        age: "",
-        },
-      :matriarch => {
-        name: "",
-        age: "",
-        },
-      :heroine => {
-        name: "",
-        age: "",
-        status: "",
-        },
+     :patriarch => {name: "Lord Capulet", age: "50"},
+     :matriarch => {name: "Lady Capulet", age: "51"},
+     :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => []
    }
   }
